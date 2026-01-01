44b9f3b22b19bc95f2eed32b38163463ea991dca
Fenyella <fenyella.derks@gmail.com>
2026-01-01 23:02:52 +0100

    Added content

---
+++ YTTimeStamper.js
@@ -0,0 +1,28 @@
+function linkFunction() {
+  let ytLink = document.getElementById("YTLink").value;
+  let hours = Number(document.getElementById("YThours").value);
+  let min = Number(document.getElementById("YTmin").value);
+  let sec = Number(document.getElementById("YTsec").value);
+
+  /*
+while (
+    Number.isNaN(hours) ||
+    Number.isNaN(min) ||
+    Number.isNaN(sec)
+) {
+    link = prompt(
+        "Try again! Link|Hours|Minutes|Seconds - The time needs to be in numbers!",
+    ).split("|");
+    ytLink = link[0];
+    hours = Number(link[1]);
+    min = Number(link[2]);
+    sec = Number(link[3]);
+    }  */
+
+  function secsWithJavaScript(hours, min, sec) {
+    return Math.floor(hours * 60 * 60 + min * 60 + sec);
+  }
+  document.getElementById("show").innerHTML =
+    ytLink + "&t=" + secsWithJavaScript(hours, min, sec) + "s";
+  console.log(ytLink + "&t=" + secsWithJavaScript(hours, min, sec) + "s");
+}
